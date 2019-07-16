33e71f57f35062e90f372fd06774cb43161380dd
Hee kun Kim <khk376@naver.com>
2019-07-16 18:04:38 +0900

    새로운 HTML 적용 및 URL 수정, 중복 제거

---
+++ my-slipp/src/main/java/net/slipp/domain/UserRepository.java
@@ -0,0 +1,8 @@
+package net.slipp.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+//데이터베이스 연결.
+
+}
--- my-slipp/src/main/java/net/slipp/web/UserController.java
+++ my-slipp/src/main/java/net/slipp/web/UserController.java
@@ -4,25 +4,34 @@ package net.slipp.web;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import net.slipp.domain.User;
+import net.slipp.domain.UserRepository;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
-	private List<User> users= new ArrayList<User>();
+	//private List<User> users= new ArrayList<User>();  db를 쓰니까 리스트는 이제 필요없음.
+	
+	@Autowired //DB 끌고오는애
+	private UserRepository userRepository;
 
-	@PostMapping("/create")
+	@PostMapping("")
 	public String create(User user) {
 		System.out.println("user :" + user);
-		users.add(user);
-		return "redirect:/list"; // 그냥 list로 하면 변경사항이 적용되지 않은 list.html로 이동됨.
+		userRepository.save(user); // jpaRepository에 기본적으로 save기능이 구현되어 있으므로 가져다 쓰기만 하면 됨
+		return "redirect:/users"; // redirect가 아닌 그냥 list로 하면 변경사항이 적용되지 않은 list.html로 이동됨.
 	}
 	
-	@GetMapping("list")
+	@GetMapping("")
 	public String list(Model model) {
-		model.addAttribute("users",users);		
-		return "list";
+		model.addAttribute("users",userRepository.findAll());	// 이제 리스트에서 데이터를 가져오는게 아니고 DB에서 데이터를 가져옴.	
+		return "/user/list";
 	}
 }
